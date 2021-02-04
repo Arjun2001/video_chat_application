78ed77c7637e5645f5170766f49cf7427b3c07f4
arjun dev pk <cb.en.u4cse18208@cb.students.amrita.edu>
2021-02-04 16:20:32 +0530

    first commit after deploy

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const url=require('url');
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
+app.use(bodyParser.urlencoded({ extended: false }))
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  req.query=url.parse(req.url,true).query;
+  console.log(req.query.id);
+  if (req.query.id) {
+    res.redirect(`/${req.query.id}`);
+  } else {
+    res.redirect(`/${uuidV4()}`);
+  }
+});
+
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT||3030);
